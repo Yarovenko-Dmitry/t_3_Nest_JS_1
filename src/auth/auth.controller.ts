6e14173653f60d942558e84fda225aa71185963e
dmitry <yarovenkojob@gmail.com>
2023-03-28 14:21:29 +0300

    feat: add loginisation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -6,8 +6,12 @@ import { AuthService } from "./auth.service";
 @ApiTags("Авторизация")
 @Controller("auth")
 export class AuthController {
-
   constructor(private authService: AuthService) {
+  }
+
+  @Post("/login")
+  login(@Body() userDto: CreateUserDto) {
+    return this.authService.login(userDto);
   }
 
   @Post("/registration")
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -11,11 +11,17 @@ export class AuthService {
               private jwtService: JwtService) {
   }
 
+  async login(userDto: CreateUserDto) {
+    const user = await this.validateUser(userDto);
+
+    return this.generateToken(user);
+  }
+
   async registration(userDto: CreateUserDto) {
     const candidate = await this.userService.getUserByEmail(userDto.email);
 
     if (candidate) {
-      throw new HttpException("Пользователь с таким email уже существует", HttpStatus.BAD_REQUEST);
+      throw new HttpException("Пользователь с таким email существует", HttpStatus.BAD_REQUEST);
     }
 
     const hashPassword = await bcrypt.hash(userDto.password, 5);
@@ -30,5 +36,16 @@ export class AuthService {
     return {
       token: this.jwtService.sign(payload)
     };
+  }
+
+  private async validateUser(userDto: CreateUserDto) {
+    const user = await this.userService.getUserByEmail(userDto.email);
+    const passwordEquals = await bcrypt.compare(userDto.password, user.password);
+
+    if (user && passwordEquals) {
+      return user;
+    }
+
+    throw new UnauthorizedException({ message: "Некорректный емайл или пароль" });
   }
 }
