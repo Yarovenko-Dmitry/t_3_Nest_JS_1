12489e5c555257b53f45554a828279b3d22aacf1
dmitry <yarovenkojob@gmail.com>
2023-03-28 14:00:09 +0300

    feat: add registration

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,5 +1,17 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { ApiTags } from "@nestjs/swagger";
+import { CreateUserDto } from "../users/dto/create-user.dto";
+import { AuthService } from "./auth.service";
 
+@ApiTags("Авторизация")
 @Controller("auth")
 export class AuthController {
+
+  constructor(private authService: AuthService) {
+  }
+
+  @Post("/registration")
+  registration(@Body() userDto: CreateUserDto) {
+    return this.authService.registration(userDto);
+  }
 }
