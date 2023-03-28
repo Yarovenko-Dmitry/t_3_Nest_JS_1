9a539a0d98bd29faa9e569d9c1300711ea5ec141
dmitry <yarovenkojob@gmail.com>
2023-03-28 10:59:11 +0300

    feat: auto generate roles files

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { SequelizeModule } from "@nestjs/sequelize";
 import { UsersModule } from "./users/users.module";
 import { ConfigModule } from "@nestjs/config";
 import { User } from "./users/users.model";
+import { RolesModule } from "./roles/roles.module";
 
 @Module({
   controllers: [],
@@ -21,7 +22,8 @@ import { User } from "./users/users.model";
       models: [User],
       autoLoadModels: true
     }),
-    UsersModule
+    UsersModule,
+    RolesModule
   ]
 })
 
